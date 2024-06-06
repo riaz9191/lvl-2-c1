28ca5c0f20a79691b50e264aaeb71146693e0dcc
riaz9191 <riazahammed9191@gmail.com>
2024-06-06 18:13:55 +0600

    concep comlete

--- src/module/movie/movie.route.ts
+++ src/module/movie/movie.route.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response } from 'express';
-import { Movie } from './movie.model';
-import { MovieController } from './movie.controller';
+import express from "express";
+import { MovieControllers } from "./movie.controller";
 
 const router = express.Router();
 
-router.post('/', MovieController.createMovie)
+router.post("/", MovieControllers.createMovie);
+router.get("/:slug", MovieControllers.getMovieBySlug);
+router.get("/", MovieControllers.getAllMovies);
 
 export const MovieRoutes = router;
